a174d9d639106dc663e911869355384855dc3be4
Ivan-Eremeev <ivan.frontcoder@gmail.com>
2019-06-05 08:29:03 +0500

    article end

--- js-app/function.js
+++ js-app/function.js
@@ -67,6 +67,9 @@ $(document).ready(function () {
 	accordeon($('#nav-accordeon'),true);
 	accordeon($('#accordeon-scroll'),true);
 
+	// Бегающее меню
+	scrollMenu($('#scrollMenu'));
+
 	// matchHeight // Задание елементам одинаковой высоты
 	// $('.match-height').matchHeight();
 
@@ -598,6 +601,82 @@ function limitBlock(wrap, newsNum) {
 			parent.append(newsLimit);
 			btn.text(btnShow)
 				.removeClass('active');
+		}
+	});
+};
+
+// Бегающее меню
+// function scrollMenu(menu) {
+// 	var offset = 20,
+// 			menuOffset = menu.offset().top;
+// 	$(window).scroll(function() {
+// 			menuOffset = $(window).scrollTop() - menuOffset;
+// 		console.log(menuOffset, menuOffset);
+// 		if (menuOffset > offset) {
+// 			menu.css({
+// 				transform: 'translate(0,'+(menuOffset+offset)+'px)'
+// 			});
+// 		}else {
+// 			menu.css({
+// 				transform: 'translate(0px)'
+// 			});
+// 		}
+// 	});
+// };
+
+function scrollMenu(menu) {
+	var offset = 30, // Отступ меню от верха страницы
+			time = 500, // Скорость прокрутки
+			menuOffset = menu.offset().top,
+			menuHeight = menu.height(),
+			wrapper = menu.parent(),
+			wrapperOffset = wrapper.offset().top,
+			wrapperHeight = wrapper.height(),
+			wrapperBottom = wrapperOffset + wrapperHeight,
+			item = menu.find('a[href^="#"]'),
+			block = $('.accordeon-scroll_block');
+	// Скролл меню вместе с экраном
+	$(window).scroll(function() {
+		var scrollTrigger = $(window).scrollTop() - menuOffset,
+				scrollMenu = scrollTrigger + offset,
+				menuBottom = scrollMenu + menuHeight;
+		if (scrollMenu > 0 && $(window).width() > breakSm) {
+			menu.css({
+				position: 'fixed',
+				top: offset
+			});
+		}else {
+			menu.css({
+				position: 'relative',
+				top: '0'
+			});
+		}
+		if (menuBottom > (wrapperHeight - 100)) {
+			menu.css({
+				top: (($(document).scrollTop() - (wrapperBottom - menuHeight - 100)) * -1)
+			});
+		}
+	});
+	// Плавный скролл
+	item.click(function(e) {
+		e.preventDefault();
+		var id = $(this).attr('href');
+				blockTop = $(id).offset().top - offset;
+		$('body,html').animate({scrollTop: blockTop}, time);
+	});
+	// Подсветка активных пунктов меню
+	$(window).scroll(function() {
+		if ($(window).width() > breakSm) {
+			block.each(function(index, el) {
+				var topBlock = $(el).offset().top - offset - 2;
+						bottomBlock = topBlock + $(el).height(),
+						scroll = $(window).scrollTop();
+						idBlock = $(el).attr('id');
+				if (scroll > topBlock && scroll < bottomBlock) {
+					item.removeClass('active');
+					menu.find('a[href="#'+idBlock+'"]').addClass('active');
+				}
+			});
 		}
 	});
 };
